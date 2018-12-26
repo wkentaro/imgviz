e3a4304cd9a67cfb3af819d16bf0f2cc5505ee5c
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-12-26 19:25:54 +0900

    Initialize imgviz

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import distutils.spawn
+from setuptools import find_packages
+from setuptools import setup
+import shlex
+import subprocess
+import sys
+
+
+version = '0.0.1'
+
+
+if sys.argv[1] == 'release':
+    if not distutils.spawn.find_executable('twine'):
+        print(
+            'Please install twine:\n\n\tpip install twine\n',
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    commands = [
+        'git tag v{:s}'.format(version),
+        'git push origin master --tag',
+        'python setup.py sdist',
+        'twine upload dist/gdown-{:s}.tar.gz'.format(version),
+    ]
+    for cmd in commands:
+        subprocess.check_call(shlex.split(cmd))
+    sys.exit(0)
+
+
+setup(
+    name='imgviz',
+    version=version,
+    packages=find_packages(),
+    install_requires=[],
+    description='Image visualization idioms.',
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    url='http://github.com/wkentaro/imgviz',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
