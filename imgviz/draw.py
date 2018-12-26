9d662c40d2c5add9679bdcded63116e20d754f4a
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-12-26 23:00:49 +0900

    Refactor draw.rectangle

--- imgviz/draw.py
+++ imgviz/draw.py
@@ -3,12 +3,13 @@ import PIL.Image
 import PIL.ImageDraw
 
 
-def rectangle(img, aabb1, aabb2, color, fill=None, width=0):
-    img_pil = PIL.Image.fromarray(img)
-    draw = PIL.ImageDraw.ImageDraw(img_pil)
+def rectangle(src, aabb1, aabb2, color, fill=None, width=0):
+    src_pil = PIL.Image.fromarray(src)
+    draw = PIL.ImageDraw.ImageDraw(src_pil)
 
     y1, x1 = aabb1
     y2, x2 = aabb2
     draw.rectangle(xy=(x1, y1, x2, y2), fill=fill, outline=color, width=width)
 
-    return np.asarray(img_pil)
+    dst = np.asarray(src_pil)
+    return dst
