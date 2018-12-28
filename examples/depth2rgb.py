546cce171fc80fb8afb9ee4b8e8e60fd48424848
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-12-28 13:41:44 +0900

    Update examples/depth2rgb.py

--- examples/depth2rgb.py
+++ examples/depth2rgb.py
@@ -11,11 +11,22 @@ here = osp.dirname(osp.abspath(__file__))
 if __name__ == '__main__':
     data = imgviz.data.arc2017()
 
-    depth = data['depth']
+    depthviz = imgviz.depth2rgb(data['depth'], min_value=0.3, max_value=1)
 
-    color = imgviz.depth2rgb(depth, min_value=0.3, max_value=1)
-    plt.imshow(color)
+    # -------------------------------------------------------------------------
+
+    fig = plt.figure(dpi=150)
+
+    plt.subplot(121)
+    plt.title('rgb')
+    plt.imshow(data['rgb'])
     plt.axis('off')
+
+    plt.subplot(122)
+    plt.title('depth (colorized)')
+    plt.imshow(depthviz)
+    plt.axis('off')
+
     out_file = osp.join(here, '.readme/depth2rgb.jpg')
     plt.savefig(
         out_file, bbox_inches='tight', transparent='True', pad_inches=0
