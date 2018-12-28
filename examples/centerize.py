1b3212868522f2cfe3cc915c35bfecd10c74d093
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-12-28 13:37:04 +0900

    Update examples/centerize.py

--- examples/centerize.py
+++ examples/centerize.py
@@ -16,16 +16,23 @@ if __name__ == '__main__':
     H, W = rgb.shape[:2]
     centerized1 = imgviz.centerize(rgb, shape=(H, H))
 
-    rgb = rgb.transpose(1, 0, 2)
-    centerized2 = imgviz.centerize(rgb, shape=(H, H))
+    rgb_T = rgb.transpose(1, 0, 2)
+    centerized2 = imgviz.centerize(rgb_T, shape=(H, H))
 
     fig = plt.figure(dpi=150)
-    plt.subplot(121)
-    plt.title('{}'.format(centerized1.shape))
+
+    plt.subplot(131)
+    plt.title('original')
+    plt.axis('off')
+    plt.imshow(rgb)
+
+    plt.subplot(132)
+    plt.title('centerized1:\n{}'.format(centerized1.shape))
     plt.imshow(centerized1)
     plt.axis('off')
-    plt.subplot(122)
-    plt.title('{}'.format(centerized2.shape))
+
+    plt.subplot(133)
+    plt.title('centerized2:\n{}'.format(centerized2.shape))
     plt.imshow(centerized2)
     plt.axis('off')
 
