9ab274a8e6748f55539b344b08f94f3aaa60aa1c
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-12-28 13:43:36 +0900

    Update examples/rectangle.py

--- examples/rectangle.py
+++ examples/rectangle.py
@@ -17,13 +17,17 @@ if __name__ == '__main__':
     aabb2 = (480, 640)
     viz = imgviz.rectangle(rgb, aabb1, aabb2, color=(0, 255, 0), width=10)
 
+    # -------------------------------------------------------------------------
+
     plt.figure(dpi=150)
+
     plt.subplot(121)
     plt.title('original')
     plt.imshow(rgb)
     plt.axis('off')
+
     plt.subplot(122)
-    plt.title('{}, {}'.format(aabb1, aabb2))
+    plt.title('aabb1: {}\naabb2: {}'.format(aabb1, aabb2))
     plt.imshow(viz)
     plt.axis('off')
 
