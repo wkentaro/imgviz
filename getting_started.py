09d461d8ce8d8f9fdd3f2a53fa7b4befddd9204b
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-12-29 02:56:13 +0900

    Update getting_started.py

--- getting_started.py
+++ getting_started.py
@@ -1,3 +1,5 @@
+# flake8: noqa
+
 import os.path as osp
 
 import matplotlib.pyplot as plt
@@ -12,9 +14,11 @@ import imgviz
 # sample data of rgb, depth, class label and instance masks
 data = imgviz.data.arc2017()
 # colorize depth image with JET colormap
-depth_viz = imgviz.depth2rgb(data['depth'], min_value=0.3, max_value=1)
+depthviz = imgviz.depth2rgb(data['depth'], min_value=0.3, max_value=1)
+# colorize label image
+labelviz = imgviz.label2rgb(data['class_label'], label_names=data['class_names'])
 # tile visualization
-tiled = imgviz.tile([data['rgb'], depth_viz], border=(255, 255, 255))
+tiled = imgviz.tile([data['rgb'], depthviz, labelviz], border=(255, 255, 255))
 # }} GETTING_STARTED
 # -----------------------------------------------------------------------------
 
