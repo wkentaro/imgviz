762d03de72ba054c013acdd38cd8c78b893c1380
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-12-30 03:25:03 +0900

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -5,12 +5,14 @@ set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $HERE
 
+# check python version
 PYTHON_VERSION="$TRAVIS_PYTHON_VERSION"
 if [ "$PYTHON_VERSION" = "" ]; then
   PYTHON_VERSION="3.6"
 fi
 PYTHON_VERSION32="$(echo $PYTHON_VERSION | cut -d '.' -f 1)"
 
+# install anaconda
 if [ ! -d .anaconda$PYTHON_VERSION32 ]; then
   curl -L https://github.com/wkentaro/dotfiles/raw/master/local/bin/install_anaconda$PYTHON_VERSION32.sh | bash -s .
   source .anaconda$PYTHON_VERSION32/bin/activate
@@ -25,7 +27,7 @@ pip install hacking
 flake8 .
 
 # mypy
-if [ "$(python -c 'import sys; print(sys.version[0])')" = "3" ]; then
+if [ "$PYTHON_VERSION32" = "3" ]; then
   pip install mypy
   mypy -p imgviz --ignore-missing-imports
 fi
