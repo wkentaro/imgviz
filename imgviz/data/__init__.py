60d4fda6229bf3f2b4e85254acc6fb5cff3e1e38
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-12-30 01:48:12 +0900

    Add imgviz.data.kitti

--- imgviz/data/__init__.py
+++ imgviz/data/__init__.py
@@ -1,3 +1,4 @@
 # flake8: noqa
 
 from .arc2017 import arc2017
+from .kitti import kitti
---
+++ imgviz/data/kitti/__init__.py
@@ -0,0 +1,26 @@
+import os.path as osp
+
+import numpy as np
+
+
+here = osp.dirname(osp.abspath(__file__))
+
+
+def read_pose_file(filename):
+    with open(filename, 'r') as f:
+        transforms = []
+        for one_line in f:
+            one_line = one_line.split(' ')
+            Rt = [float(pose) for pose in one_line] + [0, 0, 0, 1]
+
+            Rt = np.reshape(np.array(Rt), (4, 4))
+            assert abs(Rt[3].sum() - 1) < 1e-5
+            transforms.append(Rt)
+    return transforms
+
+
+def kitti():
+    pose_file = osp.join(here, 'v0/00.txt')
+    transforms = read_pose_file(pose_file)
+    data = {'transforms': transforms}
+    return data
