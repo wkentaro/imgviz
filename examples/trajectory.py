8c1c9e5789781acedef583bc399d2b7b3af43623
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-12-30 01:48:12 +0900

    Add imgviz.data.kitti

--- examples/trajectory.py
+++ examples/trajectory.py
@@ -1,31 +1,15 @@
 import os.path as osp
 
-import numpy as np
-
 import imgviz
 
 
 here = osp.dirname(osp.abspath(__file__))
 
 
-def read_poses(filename):
-    with open(filename, 'r') as f:
-        transforms = []
-        for one_line in f:
-            one_line = one_line.split(' ')
-            Rt = [float(pose) for pose in one_line] + [0, 0, 0, 1]
-
-            Rt = np.reshape(np.array(Rt), (4, 4))
-            assert abs(Rt[3].sum() - 1) < 1e-5
-            transforms.append(Rt)
-    return transforms
-
-
 if __name__ == '__main__':
-    file = osp.join(here, 'data/KITTI_VO_pose/00.txt')
-    transforms = read_poses(file)
+    data = imgviz.data.kitti()
 
-    img = imgviz.plot_trajectory(transforms, is_relative=False)
+    img = imgviz.plot_trajectory(data['transforms'])
     out_file = osp.join(here, '.readme/trajectory.jpg')
     imgviz.io.imsave(out_file, img)
 
