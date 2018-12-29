1b451b52b5d6942bcb8caadc7612da920e98c329
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-12-30 02:59:13 +0900

    Move everything to .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -3,8 +3,22 @@
 set -e
 set -x
 
+# flake8
+pip install hacking
+flake8 .
+
+# mypy
+pip install mypy
+mypy -p imgviz --ignore-missing-imports
+
+# install
+pip install -e .
+
+# pytest
+pip install pytest
 pytest -v tests
 
+# examples
 MPLBACKEND=agg python getting_started.py
 for f in examples/*.py; do
   MPLBACKEND=agg python $f
