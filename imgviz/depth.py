b95cfb5b0b8e0ed841220dda3be1bd92d40d9a27
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-01-02 19:02:55 +0900

    Add docstrings for imgviz.depth

--- imgviz/depth.py
+++ imgviz/depth.py
@@ -1,3 +1,4 @@
+import numbers
 import warnings
 
 import matplotlib.cm
@@ -10,10 +11,41 @@ def depth2rgb(
     max_value=None,
     colormap='jet',
 ):
+    '''Convert depth to rgb.
+
+    Parameters
+    ----------
+    depth: numpy.ndarray, (H, W), float
+        Depth image.
+    min_value: float, optional
+        Minimum value for colorizing.
+    max_value: float, optional
+        Maximum value for colorizing.
+    colormap: str, optional
+        Colormap, default: 'jet'.
+
+    Returns
+    -------
+    rgb: numpy.ndarray, (H, W, 3), np.uint8
+        Output colorized image.
+    '''
+    assert depth.ndim == 2, 'depth image must be 2 dimensional'
+    assert np.issubdtype(depth.dtype, np.floating), 'depth dtype must be float'
+
     if min_value is None:
         min_value = np.nanmin(depth)
+    else:
+        assert isinstance(min_value, numbers.Real), \
+            'min_value must be float type'
+
     if max_value is None:
         max_value = np.nanmax(depth)
+    else:
+        assert isinstance(max_value, numbers.Real), \
+            'max_value must be float type'
+
+    assert hasattr(matplotlib.cm, colormap), \
+        'unsupported colormap: {}'.format(colormap)
 
     if np.isinf(min_value) or np.isinf(max_value):
         warnings.warn('Min or max value for depth colorization is inf.')
