783bd700a96fcc0bc8ff4f14e9a2d2870e515b31
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-01-14 16:04:58 +0000

    Support resizing float nchannel image with pillow

--- imgviz/resize.py
+++ imgviz/resize.py
@@ -7,6 +7,54 @@ import numpy as np
 import PIL.Image
 
 
+def _resize_pillow(src, height, width, interpolation):
+    if interpolation == 'linear':
+        interpolation = PIL.Image.LINEAR
+    elif interpolation == 'nearest':
+        interpolation = PIL.Image.NEAREST
+    else:
+        raise ValueError(
+            'unsupported interpolation: {}'.format(interpolation)
+        )
+
+    if src.dtype == np.uint8:
+        dst = PIL.Image.fromarray(src)
+        dst = dst.resize((width, height), resample=interpolation)
+        dst = np.asarray(dst)
+    else:
+        assert np.issubdtype(src.dtype, np.floating)
+        ndim = src.ndim
+        if ndim == 2:
+            src = src[:, :, None]
+
+        C = src.shape[2]
+        dst = np.zeros((height, width, C), dtype=src.dtype)
+        for c in range(C):
+            src_c = src[:, :, c]
+            src_c = PIL.Image.fromarray(src_c)
+            dst[:, :, c] = src_c.resize(
+                (width, height), resample=interpolation
+            )
+
+        if ndim == 2:
+            dst = dst[:, :, 0]
+    return dst
+
+
+def _resize_opencv(src, height, width, interpolation):
+    if interpolation == 'linear':
+        interpolation = cv2.INTER_LINEAR
+    elif interpolation == 'nearest':
+        interpolation = cv2.INTER_NEAREST
+    else:
+        raise ValueError(
+            'unsupported interpolation: {}'.format(interpolation)
+        )
+
+    dst = cv2.resize(src, (width, height), interpolation=interpolation)
+    return dst
+
+
 def resize(
     src,
     height=None,
@@ -37,29 +85,9 @@ def resize(
         width = int(round(scale_width * src_width))
 
     if backend == 'pillow':
-        if interpolation == 'linear':
-            interpolation = PIL.Image.LINEAR
-        elif interpolation == 'nearest':
-            interpolation = PIL.Image.NEAREST
-        else:
-            raise ValueError(
-                'unsupported interpolation: {}'.format(interpolation)
-            )
-
-        src = PIL.Image.fromarray(src)
-        dst = src.resize((width, height), interpolation)
-        dst = np.asarray(dst)
+        dst = _resize_pillow(src, height, width, interpolation)
     elif backend == 'opencv':
-        if interpolation == 'linear':
-            interpolation = cv2.INTER_LINEAR
-        elif interpolation == 'nearest':
-            interpolation = cv2.INTER_NEAREST
-        else:
-            raise ValueError(
-                'unsupported interpolation: {}'.format(interpolation)
-            )
-
-        dst = cv2.resize(src, (width, height), interpolation=interpolation)
+        dst = _resize_opencv(src, height, width, interpolation)
     else:
         raise ValueError('unsupported backend: {}'.format(backend))
 
