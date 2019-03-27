9628bb7c1c9a047f15a1f9e94e639530c4267265
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-03-27 04:47:05 +0000

    Support generator and list by pyglet_imshow

---
+++ checks/_io_checks/_pyglet_checks/check_pyglet_imshow.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import imgviz
+
+
+def get_images():
+    data = imgviz.data.arc2017()
+    yield data['rgb']
+    yield imgviz.depth2rgb(data['depth'], min_value=0.3, max_value=1)
+    yield imgviz.label2rgb(data['class_label'])
+
+
+def main():
+    imgviz.io.pyglet_imshow(next(get_images()), 'ndarray')
+    imgviz.io.pyglet_run()
+
+    imgviz.io.pyglet_imshow(get_images(), 'generator')
+    imgviz.io.pyglet_run()
+
+    imgviz.io.pyglet_imshow(list(get_images()), 'list')
+    imgviz.io.pyglet_run()
+
+
+if __name__ == '__main__':
+    main()
--- imgviz/_io/_pyglet.py
+++ imgviz/_io/_pyglet.py
@@ -1,4 +1,6 @@
-import numpy as np  # NOQA
+import types
+
+import numpy as np
 import PIL.Image
 
 try:
@@ -14,9 +16,156 @@ def check_pyglet_available():
         )
 
 
-def pyglet_imshow(image, caption=None):
+def pyglet_run():
     check_pyglet_available()
 
+    return pyglet.app.run()
+
+
+def pyglet_imshow(image, caption=None, **kwargs):
+    check_pyglet_available()
+
+    if isinstance(image, types.GeneratorType):
+        _pyglet_imshow_generator(image, caption=caption, **kwargs)
+    elif isinstance(image, np.ndarray):
+        _pyglet_imshow_ndarray(image, caption=caption)
+    else:
+        _pyglet_imshow_list(image, caption=caption, **kwargs)
+
+
+def _pyglet_imshow_list(images, caption, interval=0.5):
+    index = 0
+
+    image = _ndarray_to_imagedata(images[index])
+    window, sprite = _window_and_sprite(image, caption=caption)
+
+    window.index = index
+    window.play = False
+
+    @window.event
+    def on_draw():
+        window.clear()
+        sprite.draw()
+
+    def usage():
+        print('Usage: ')
+        print('  h: show help')
+        print('  q: close window')
+        print('  n: next image')
+        print('  p: previous image')
+        print('  s: toggle play')
+
+    def play_callback(dt):
+        if window.play:
+            if window.index == len(images) - 1:
+                print("Press 'q' to quit")
+                window.play = False
+            window.index = min(window.index + 1, len(images) - 1)
+            sprite.image = _ndarray_to_imagedata(images[window.index])
+
+    pyglet.clock.schedule_interval(play_callback, interval)
+
+    @window.event()
+    def on_key_press(symbol, modifiers):
+        if modifiers == 0:
+            if symbol == pyglet.window.key.Q:
+                window.close()
+            elif symbol == pyglet.window.key.N:
+                window.index = min(window.index + 1, len(images) - 1)
+                sprite.image = _ndarray_to_imagedata(images[window.index])
+            elif symbol == pyglet.window.key.P:
+                window.index = max(window.index - 1, 0)
+                sprite.image = _ndarray_to_imagedata(images[window.index])
+            elif symbol == pyglet.window.key.S:
+                window.play = not window.play
+            elif symbol == pyglet.window.key.H:
+                usage()
+            else:
+                print("Press 'h' to show help")
+
+
+def _pyglet_imshow_generator(images, caption, interval=0.5):
+    image = _ndarray_to_imagedata(next(images))
+    window, sprite = _window_and_sprite(image, caption=caption)
+
+    window.play = False
+
+    @window.event
+    def on_draw():
+        window.clear()
+        sprite.draw()
+
+    def usage():
+        print('Usage: ')
+        print('  h: show help')
+        print('  q: close window')
+        print('  n: next image')
+        print('  s: toggle play')
+
+    def play_callback(dt):
+        if window.play:
+            try:
+                sprite.image = _ndarray_to_imagedata(next(images))
+            except StopIteration:
+                print("Press 'q' to quit")
+                window.play = False
+
+    pyglet.clock.schedule_interval(play_callback, interval)
+
+    @window.event()
+    def on_key_press(symbol, modifiers):
+        if modifiers == 0:
+            if symbol == pyglet.window.key.Q:
+                window.close()
+            elif symbol == pyglet.window.key.N:
+                try:
+                    sprite.image = _ndarray_to_imagedata(next(images))
+                except StopIteration:
+                    print("Press 'q' to quit")
+            elif symbol == pyglet.window.key.S:
+                window.play = not window.play
+            elif symbol == pyglet.window.key.H:
+                usage()
+            else:
+                print("Press 'h' to show help")
+
+
+def _pyglet_imshow_ndarray(image, caption):
+    image = _ndarray_to_imagedata(image)
+    window, sprite = _window_and_sprite(image, caption=caption)
+
+    @window.event
+    def on_draw():
+        window.clear()
+        sprite.draw()
+
+    def usage():
+        print('Usage: ')
+        print('  h: show help')
+        print('  q: close window')
+
+    @window.event()
+    def on_key_press(symbol, modifiers):
+        if modifiers == 0:
+            if symbol == pyglet.window.key.Q:
+                window.close()
+            elif symbol == pyglet.window.key.H:
+                usage()
+            else:
+                print("Press 'h' to show help")
+
+
+def _window_and_sprite(image, caption):
+    window = pyglet.window.Window(
+        width=image.width,
+        height=image.height,
+        caption=caption,
+    )
+    sprite = pyglet.sprite.Sprite(image)
+    return window, sprite
+
+
+def _ndarray_to_imagedata(image):
     image = PIL.Image.fromarray(image)
     image = pyglet.image.ImageData(
         width=image.width,
@@ -25,43 +174,4 @@ def pyglet_imshow(image, caption=None):
         data=image.tobytes(),
         pitch=- image.width * len(image.mode),
     )
-
-    window = pyglet.window.Window(
-        width=image.width,
-        height=image.height,
-        caption=caption,
-        # resizable=True,
-    )
-    sprite = pyglet.sprite.Sprite(image)
-
-    @window.event
-    def on_draw():
-        window.clear()
-        sprite.draw()
-
-    @window.event()
-    def on_key_press(symbol, modifiers):
-        if modifiers == 0:
-            if symbol == pyglet.window.key.Q:
-                window.close()
-
-    # @window.event()
-    # def on_resize(width, height):
-    #     scale_w = 1. * width / image.width
-    #     scale_h = 1. * height / image.height
-    #
-    #     image_scale = min(scale_w, scale_h)
-    #     image_width = int(round(image.width * sprite.scale))
-    #     image_height = int(round(image.height * sprite.scale))
-    #
-    #     sprite.update(
-    #         x=(width - image_width) / 2.0,
-    #         y=(height - image_height) / 2.0,
-    #         scale=image_scale,
-    #     )
-
-
-def pyglet_run():
-    check_pyglet_available()
-
-    return pyglet.app.run()
+    return image
