4da76d8eaaa1cefbec311cfe97200cab4857a585
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-06-28 14:33:22 +0100

    Add data.lena

--- imgviz/data/__init__.py
+++ imgviz/data/__init__.py
@@ -2,6 +2,8 @@
 
 from .arc2017 import arc2017
 
+from .lena import lena
+
 from .middlebury import middlebury
 
 from .kitti import kitti_odometry
---
+++ imgviz/data/lena/__init__.py
@@ -0,0 +1,12 @@
+import os.path as osp
+
+from ..._io import imread
+
+
+here = osp.dirname(osp.abspath(__file__))
+
+
+def lena():
+    image_file = osp.join(here, 'lena.png')
+    image = imread(image_file)
+    return image
