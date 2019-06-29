cc303bac87f7a5f28983507be61e68e58bc9119f
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-06-29 19:15:59 +0100

    Add draw.star

--- imgviz/draw.py
+++ imgviz/draw.py
@@ -7,6 +7,69 @@ import PIL.ImageDraw
 import PIL.ImageFont
 
 
+def star(src, center, size, fill=None, outline=None):
+    '''Draw star on numpy array with Pillow.
+
+    Parameters
+    ----------
+    src: numpy.ndarray
+        Input image.
+    center: (2,) array-like
+        center is (cy, cx).
+    size: float
+        Diameter to create the star.
+    fill: (3,) array-like, optional
+        RGB color to fill the mark. None for no fill. (default: None)
+    outline: (3,) array-like, optional
+        RGB color to draw the outline.
+
+    Returns
+    -------
+    dst: numpy.ndarray
+        Output image.
+    '''
+    dst = PIL.Image.fromarray(src)
+    draw = PIL.ImageDraw.Draw(dst)
+
+    radius = size / 2
+    cy, cx = center
+
+    # 5 mountains
+    angles_m = np.arange(0, 5) * np.pi * 2 / 5 + (np.pi / 2)
+    x_m = cx + radius * np.cos(angles_m)
+    y_m = cy - radius * np.sin(angles_m)
+    xy_m = np.stack((x_m, y_m), axis=1)
+
+    # 5 valleys
+    angles_v = angles_m + np.pi / 5
+    l = radius * 1 / (
+        np.sin(np.pi / 5) / np.tan(np.pi / 10) + np.cos(np.pi / 10)
+    )
+    x_v = cx + l * np.cos(angles_v)
+    y_v = cy - l * np.sin(angles_v)
+    xy_v = np.stack((x_v, y_v), axis=1)
+
+    xy = np.array([
+        xy_m[0],
+        xy_v[0],
+        xy_m[1],
+        xy_v[1],
+        xy_m[2],
+        xy_v[2],
+        xy_m[3],
+        xy_v[3],
+        xy_m[4],
+        xy_v[4],
+        xy_m[0],
+    ])
+    xy = xy.flatten().tolist()
+    draw.polygon(xy, fill=fill, outline=outline)
+
+    dst = np.asarray(dst)
+    dst.setflags(write=True)
+    return dst
+
+
 def circle(src, center, diameter, fill=None, outline=None, width=0):
     '''Draw circle on numpy array with Pillow.
 
@@ -16,8 +79,10 @@ def circle(src, center, diameter, fill=None, outline=None, width=0):
         Input image.
     center: (2,) array-like
         center is (cy, cx).
+    diameter: float
+        Diameter of the circle.
     fill: (3,) array-like, optional
-        RGB color to fill the circle. None for no fill. (default: None)
+        RGB color to fill the mark. None for no fill. (default: None)
     outline: (3,) array-like, optional
         RGB color to draw the outline.
     width: int, optional
@@ -33,7 +98,7 @@ def circle(src, center, diameter, fill=None, outline=None, width=0):
 
     cy, cx = center
 
-    radius = diameter / 2
+    radius = diameter / 2.
     x1 = cx - radius
     x2 = x1 + diameter
     y1 = cy - radius
@@ -56,9 +121,9 @@ def rectangle(src, aabb1, aabb2, fill=None, outline=None, width=0):
     aabb1, aabb2: (2,) array-like
         aabb1 is (y_min, x_min) and aabb2 is (y_max, x_max).
     fill: (3,) array-like, optional
-        RGB color to fill the rectangle. None for no fill. (default: None)
-    outline: (3,) array-like
-        RGB color of the outline in uint8.
+        RGB color to fill the mark. None for no fill. (default: None)
+    outline: (3,) array-like, optional
+        RGB color to draw the outline.
     width: int, optional
         Rectangle line width. (default: 0)
 
