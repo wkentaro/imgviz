0e23206cd84e104d487a987a30eed20b549cd718
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-02-06 14:43:46 +0000

    Refactor imgviz.io.pyglet

---
+++ imgviz/_io/_pyglet/base.py
@@ -0,0 +1,12 @@
+try:
+    import pyglet
+except ImportError:
+    pyglet = None
+
+
+def check_pyglet_available():
+    if pyglet is None:
+        raise ImportError(
+            'pyglet is not installed, run following: pip install pyglet'
+        )
+    return pyglet
