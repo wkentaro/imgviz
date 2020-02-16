21c4cd9ead348bcbde3be1f445979acd0c33a7e0
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-02-16 19:51:16 +0000

    Apply black --line-length 90 getting_started.py

--- getting_started.py
+++ getting_started.py
@@ -16,26 +16,26 @@ import imgviz
 data = imgviz.data.arc2017()
 
 # colorize depth image with JET colormap
-depth = data['depth']
+depth = data["depth"]
 depthviz = imgviz.depth2rgb(depth, min_value=0.3, max_value=1)
 
 # colorize label image
-class_label = data['class_label']
-labelviz = imgviz.label2rgb(class_label, label_names=data['class_names'])
+class_label = data["class_label"]
+labelviz = imgviz.label2rgb(class_label, label_names=data["class_names"])
 
 # instance bboxes
-rgb = data['rgb']
-bboxes = data['bboxes'].astype(int)
-labels = data['labels']
-captions = [data['class_names'][l] for l in labels]
+rgb = data["rgb"]
+bboxes = data["bboxes"].astype(int)
+labels = data["labels"]
+captions = [data["class_names"][l] for l in labels]
 bboxviz = imgviz.instances2rgb(image=rgb, bboxes=bboxes, labels=labels, captions=captions)
 
 # instance masks
-masks = data['masks'] == 1
+masks = data["masks"] == 1
 maskviz = imgviz.instances2rgb(image=rgb, masks=masks, labels=labels, captions=captions)
 
 # tile instance masks
-insviz = [(rgb * m[:, :, None])[b[0]:b[2], b[1]:b[3]] for b, m in zip(bboxes, masks)]
+insviz = [(rgb * m[:, :, None])[b[0] : b[2], b[1] : b[3]] for b, m in zip(bboxes, masks)]
 insviz = imgviz.tile(imgs=insviz, border=(255, 255, 255))
 
 # tile visualization
@@ -47,10 +47,10 @@ tiled = imgviz.tile(
 # }} GETTING_STARTED
 # -----------------------------------------------------------------------------
 
-out_file = osp.join(here, '.readme/getting_started.jpg')
+out_file = osp.join(here, ".readme/getting_started.jpg")
 imgviz.io.imsave(out_file, tiled)
 
 img = imgviz.io.imread(out_file)
 plt.imshow(img)
-plt.axis('off')
+plt.axis("off")
 plt.show()
