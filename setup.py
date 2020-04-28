1fdb90a480a7c10c04fc1811d7a0fc58359e81aa
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-04-28 18:57:34 +0100

    Drop Python2 support

--- setup.py
+++ setup.py
@@ -84,11 +84,9 @@ def main():
         name="imgviz",
         version=version,
         packages=find_packages(exclude=["github2pypi"]),
+        python_requires=">=3.5",
         install_requires=get_install_requires(),
-        # pyglet>1.5.0 won't support Python2
-        extras_require={
-            "all": ["pyglet<=1.5.0", "scikit-image", "scikit-learn"]
-        },
+        extras_require={"all": ["pyglet", "scikit-image", "scikit-learn"]},
         description="Image Visualization Tools",
         long_description=get_long_description(),
         long_description_content_type="text/markdown",
@@ -101,14 +99,14 @@ def main():
         classifiers=[
             "Development Status :: 5 - Production/Stable",
             "Intended Audience :: Developers",
-            "Natural Language :: English",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
             "Programming Language :: Python",
-            "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: Implementation :: CPython",
-            "Programming Language :: Python :: Implementation :: PyPy",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3 :: Only",
         ],
     )
 
