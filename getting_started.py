5d57bcaccc78d04994741d3701697f7abdff54e3
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-06-17 16:33:34 +0900

    Update getting_started.py

--- getting_started.py
+++ getting_started.py
@@ -15,28 +15,31 @@ import imgviz
 # sample data of rgb, depth, class label and instance masks
 data = imgviz.data.arc2017()
 
+rgb = data["rgb"]
+gray = imgviz.rgb2gray(rgb)
+
 # colorize depth image with JET colormap
 depth = data["depth"]
 depthviz = imgviz.depth2rgb(depth, min_value=0.3, max_value=1)
 
 # colorize label image
 class_label = data["class_label"]
-labelviz = imgviz.label2rgb(class_label, label_names=data["class_names"], font_size=20)
+labelviz = imgviz.label2rgb(class_label, image=gray, label_names=data["class_names"], font_size=20)
 
 # instance bboxes
-rgb = data["rgb"]
 bboxes = data["bboxes"].astype(int)
 labels = data["labels"]
 captions = [data["class_names"][l] for l in labels]
-bboxviz = imgviz.instances2rgb(image=rgb, bboxes=bboxes, labels=labels, captions=captions)
+bboxviz = imgviz.instances2rgb(rgb, bboxes=bboxes, labels=labels, captions=captions)
 
 # instance masks
 masks = data["masks"] == 1
-maskviz = imgviz.instances2rgb(image=rgb, masks=masks, labels=labels, captions=captions)
+maskviz = imgviz.instances2rgb(gray, masks=masks, labels=labels, captions=captions)
 
 # tile instance masks
 insviz = [(rgb * m[:, :, None])[b[0] : b[2], b[1] : b[3]] for b, m in zip(bboxes, masks)]
 insviz = imgviz.tile(imgs=insviz, border=(255, 255, 255))
+insviz = imgviz.resize(insviz, height=rgb.shape[0])
 
 # tile visualization
 tiled = imgviz.tile(
