ce361e76e96652defefc559a416b7e3207f805f7
Jie Yuan <j.yuan@enlite.ai>
2021-10-06 16:15:18 +0200

    broadcast alpha of label2rgb to list

--- tests/test_label.py
+++ tests/test_label.py
@@ -22,3 +22,20 @@ def test_label2rgb():
     )
     assert labelviz.dtype == np.uint8
     assert labelviz.shape == (H, W, 3)
+
+    labelviz = imgviz.label2rgb(
+        label=data["class_label"],
+        image=data["rgb"],
+        label_names=data["class_names"],
+        alpha=0.5
+    )
+    assert labelviz.dtype == np.uint8
+    assert labelviz.shape == (H, W, 3)
+    labelviz = imgviz.label2rgb(
+        label=data["class_label"],
+        image=data["rgb"],
+        label_names=data["class_names"],
+        alpha=[0.5 for i in range(np.max(data["class_label"]) + 1)]
+    )
+    assert labelviz.dtype == np.uint8
+    assert labelviz.shape == (H, W, 3)
