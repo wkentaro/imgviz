46d37da2245775c86b33a0ef54974086f0a274c6
Kentaro Wada <www.kentaro.wada@gmail.com>
2023-02-08 00:36:13 +0900

    Remove PygletThreadedImageViewer
    - unstable
    - slow

--- imgviz/_io/_pyglet/__init__.py
+++ imgviz/_io/_pyglet/__init__.py
@@ -1,13 +1,4 @@
 # flake8: noqa
 
-from .base import check_pyglet_available
 from .pyglet_imshow import pyglet_imshow
 from .pyglet_run import pyglet_run
-
-try:
-    from .pyglet_threaded_image_viewer import PygletThreadedImageViewer
-except Exception:
-
-    class PygletThreadedImageViewer(object):  # type: ignore
-        def __init__(*args, **kwargs):
-            from .pyglet_threaded_image_viewer import PygletThreadedImageViewer
