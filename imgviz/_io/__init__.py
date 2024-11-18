5d1b402ac22f86c41fc804e80645736ee86f9d8d
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-11-19 08:06:16 +0900

    Add imgviz.io.lblsave

--- imgviz/_io/__init__.py
+++ imgviz/_io/__init__.py
@@ -4,6 +4,7 @@ from ._pyglet import pyglet_imshow
 from ._pyglet import pyglet_run
 from .base import imread
 from .base import imsave
+from .base import lblsave
 from .opencv import cv_imshow
 from .opencv import cv_waitkey
 from .pil import pil_imshow
