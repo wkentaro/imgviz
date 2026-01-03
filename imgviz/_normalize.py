6f9656d31186038b5b76d01ee047c85353db9d60
Kentaro Wada <www.kentaro.wada@gmail.com>
2026-01-03 21:27:05 +0900

    refactor(normalize): improve type hints

--- imgviz/_normalize.py
+++ imgviz/_normalize.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import typing
 import warnings
+from collections.abc import Sequence
 from typing import Literal
 
 import numpy as np
@@ -11,27 +12,30 @@ from numpy.typing import NDArray
 @typing.overload
 def normalize(
     src: ...,
-    min_value: float | NDArray | None = ...,
-    max_value: float | NDArray | None = ...,
+    min_value: float | Sequence[float] | NDArray[np.floating] | None = ...,
+    max_value: float | Sequence[float] | NDArray[np.floating] | None = ...,
     return_minmax: Literal[False] = ...,
-) -> NDArray: ...
+) -> NDArray[np.float32]: ...
 
 
 @typing.overload
 def normalize(
     src: ...,
-    min_value: float | NDArray | None = ...,
-    max_value: float | NDArray | None = ...,
+    min_value: float | Sequence[float] | NDArray[np.floating] | None = ...,
+    max_value: float | Sequence[float] | NDArray[np.floating] | None = ...,
     return_minmax: Literal[True] = ...,
-) -> tuple[NDArray, NDArray, NDArray]: ...
+) -> tuple[NDArray[np.float32], NDArray[np.float32], NDArray[np.float32]]: ...
 
 
 def normalize(
     src: NDArray,
-    min_value: float | NDArray | None = None,
-    max_value: float | NDArray | None = None,
+    min_value: float | Sequence[float] | NDArray[np.floating] | None = None,
+    max_value: float | Sequence[float] | NDArray[np.floating] | None = None,
     return_minmax: bool = False,
-) -> NDArray | tuple[NDArray, NDArray, NDArray]:
+) -> (
+    NDArray[np.float32]
+    | tuple[NDArray[np.float32], NDArray[np.float32], NDArray[np.float32]]
+):
     """Normalize image.
 
     Args:
@@ -52,13 +56,13 @@ def normalize(
 
     if min_value is None:
         min_value = np.nanmin(src, axis=(0, 1))
-    min_value = np.atleast_1d(min_value).astype(float)
+    min_value = np.atleast_1d(min_value).astype(np.float32)
     if min_value.shape != (D,):
         raise ValueError(f"min_value.shape must be ({D},), but got {min_value.shape}")
 
     if max_value is None:
         max_value = np.nanmax(src, axis=(0, 1))
-    max_value = np.atleast_1d(max_value).astype(float)
+    max_value = np.atleast_1d(max_value).astype(np.float32)
     if max_value.shape != (D,):
         raise ValueError(f"max_value.shape must be ({D},), but got {max_value.shape}")
 
@@ -70,7 +74,7 @@ def normalize(
     min_value[issame] -= eps
     max_value[issame] += eps
 
-    dst = np.zeros(src.shape, dtype=float)
+    dst: NDArray[np.float32] = np.zeros(src.shape, dtype=np.float32)
 
     if src.ndim == 2:
         isnan = np.isnan(src)
