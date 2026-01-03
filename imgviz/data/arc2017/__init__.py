53ec185e072c9cb4c41af9a04c89aa54ee91cb6c
Kentaro Wada <www.kentaro.wada@gmail.com>
2026-01-03 09:45:45 +0900

    refactor(data/arc2017): modernize with pathlib and type hints

--- imgviz/data/arc2017/__init__.py
+++ imgviz/data/arc2017/__init__.py
@@ -1,31 +1,29 @@
-# flake8: noqa
-
 import json
-import os.path as osp
+import pathlib
 
 import numpy as np
 
-here = osp.dirname(osp.abspath(__file__))
+_here: pathlib.Path = pathlib.Path(__file__).parent
 
 
-def arc2017():
-    data_file = osp.join(here, "data.npz")
-    data = dict(np.load(data_file))
+def arc2017() -> dict:
+    data_file: pathlib.Path = _here / "data.npz"
+    data: dict = dict(np.load(data_file))
 
     # compose masks to class label image
     class_label = np.full(data["rgb"].shape[:2], 0, dtype=np.int32)
-    for l, mask in zip(data["labels"], data["masks"]):
-        class_label[mask == 1] = l
+    for label_id, mask in zip(data["labels"], data["masks"]):
+        class_label[mask == 1] = label_id
     data["class_label"] = class_label
 
-    names_file = osp.join(here, "class_names.txt")
+    names_file: pathlib.Path = _here / "class_names.txt"
     with open(names_file) as f:
         class_names = [name.strip() for name in f]
     data["class_names"] = class_names
 
-    data["res4"] = np.load(osp.join(here, "res4.npz"))["res4"]
+    data["res4"] = np.load(_here / "res4.npz")["res4"]
 
-    with open(osp.join(here, "camera_info.json")) as f:
+    with open(_here / "camera_info.json") as f:
         data["camera_info"] = json.load(f)
 
     return data
